5785aafeb72d98417cf742ac31d89cedcd2d606b
Sharif33 <sharif.cse.nu@gmail.cm>
2022-01-04 09:32:25 +0600

    photos edit

--- src/Components/Pages/Photos/Photos/Photo.js
+++ src/Components/Pages/Photos/Photos/Photo.js
@@ -1,22 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Photo = ({photo}) => {
-    const {url,paidSrc,freeSrc}=photo;
+    const {url}=photo;
     return (
         <div>
 
             <div className="col">
-                <div>
-                   
-                        <img className='img-fluid' src={paidSrc} alt="" />
-                        <img className='img-fluid' src={freeSrc} alt="" />
-                        <img className='img-fluid' src={url} alt="" />
-                   
-                    {/* <div className="px-4 d-flex justify-content-between">
-                        <Link to={`/mobile/${_id}`}><button className="btn btn-custom-2">OVERVIEW</button></Link>
-                        <Link to={`/mobile2/${_id}`}><button className="btn btn-custom">BUY NOW</button></Link>
-                    </div> */}
-                </div>
+                       <div>
+                       <img style={{height:"20rem"}} className='img-fluid w-100' src={url} alt="" />
+                       <div className="px-4 d-flex justify-content-between">
+                        <Link to={`mobiles`}><button className="btn btn-custom-2">OVERVIEW</button></Link>
+                        <Link to={`/mobile2`}><button className="btn btn-custom">BUY NOW</button></Link>
+                    </div>  
+                       </div>
             </div>
         </div>
     );
--- src/Components/Pages/Photos/Photos/Photos.js
+++ src/Components/Pages/Photos/Photos/Photos.js
@@ -1,29 +1,63 @@
 import { CircularProgress } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import Photo from './Photo';
+import "./Photos.css";
 
 const Photos = () => {
+    const [categories, setCategories] = useState([]);
     const [photos, setPhotos] = useState([]);
 
     useEffect(() => {
         fetch(`https://raw.githubusercontent.com/h-zahar/picshore-reactjs/structure/public/photo.json`)
             .then(res => res.json())
-            .then(data => setPhotos(data))
+            .then(data =>{
+                setPhotos(data);
+                setCategories(data);
+            } )
     }, [])
+// Filter
+const filterResult = (paidPhotos) => {
+    const result = photos.filter(currentData => {
+        return currentData.option === paidPhotos;
+    });
+    setCategories(result);
+    // console.log(result)
+}
+
+// Search
+    const handleSearch = e =>{
+        const searchText = e.target.value;
+        const result = photos.filter(photo=>photo.category.toLowerCase().includes(searchText.toLowerCase()));
+        setCategories(result);
+}
     return (
         <div className='bg-light'>
+            <div className="bannerTop">
+                <div className="bannerInput text-center p-4 text-light">
+                    <h1>Graphic resources for everyone</h1>
+                    <h2>Download High-Quality Images. Free Stock Photos, Vectors & PSD</h2>
+                   <div className="text-center pt-2"> 
+                        <input placeholder='Search blood group' className='custom-input w-50' type="text" onChange={handleSearch} />
+                        <p>Examples:
+Flyer T shirt mockup Happy new year 2022</p>
+                    </div> 
+                    <button onClick={() => filterResult('Paid')} className='btn btn-danger mx-2'>PAID</button>
+                    <button onClick={() => filterResult('Free')} className='btn btn-danger mx-2'>FREE</button>
+                </div>
+            </div>
             <div className="container">
                 <div className="text-center pt-2">
                     <h4 className="fw-bold text-primary p-2">Feature Photo</h4>
                     <hr />
                 </div>
+                
                 <div className="row row-cols-1 row-cols-md-3">
-                    {photos.length == 0 ?
+                    {categories.length == 0 ?
                         <div className="w-100 text-center">
                             <CircularProgress />
                         </div>
                         :
-                        photos.map(photo => <Photo
+                        categories.map(photo => <Photo
                             key={photo.id}
                             photo={photo}
                         />)
