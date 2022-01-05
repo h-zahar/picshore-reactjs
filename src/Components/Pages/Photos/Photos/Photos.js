fd98b2df0f98316e932681fc3f980571c1c65624
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2022-01-05 21:24:34 +0600

    Fixed Import Issue

--- src/Components/Pages/Photos/Photos/Photos.js
+++ src/Components/Pages/Photos/Photos/Photos.js
@@ -1,6 +1,6 @@
 import { CircularProgress } from '@mui/material';
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { Link } from 'react-router-dom';
 import Photo from './Photo';
 import "./Photos.css";
 
